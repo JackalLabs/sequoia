a57695de984bac8e0b1abadb110a46e24d88183a
Daniel Ahn <dahn510@gmail.com>
2024-06-25 17:40:00 -0700

    add migrate cmd

---
+++ cmd/migrate.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/JackalLabs/sequoia/cmd/types"
+	"github.com/JackalLabs/sequoia/core"
+	"github.com/spf13/cobra"
+)
+
+func JprovMigrateCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "migrate-jprov",
+		Short:   "migrate jprov storage to sequoia",
+		Long:    "Migrate from jackal-provider storage to sequoia. It does not move files, it creates a copy to sequoia ipfs database.",
+		Example: "migrate-jprov $HOME/.jackal-storage",
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			home, err := cmd.Flags().GetString(types.FlagHome)
+			if err != nil {
+				return err
+			}
+
+			jprovRootDir := args[0]
+
+			app := core.NewApp(home)
+
+			go app.Migrate(jprovRootDir)
+
+			app.Start()
+
+			return nil
+		},
+	}
+}
