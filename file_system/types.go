22a611c53495d3a97d881ff94e19976ea67a87f8
marston <marston@jackallabs.io>
2024-07-10 14:49:10 -0400

    adding ipfs peer api

--- file_system/types.go
+++ file_system/types.go
@@ -3,22 +3,25 @@ package file_system
 import (
 	"context"
 
+	"github.com/libp2p/go-libp2p/core/host"
+
 	ipfs2 "github.com/JackalLabs/sequoia/ipfs"
 	"github.com/dgraph-io/badger/v4"
 	ipfslite "github.com/hsanjuan/ipfs-lite"
 )
 
 type FileSystem struct {
-	db   *badger.DB
-	ipfs *ipfslite.Peer
+	db       *badger.DB
+	ipfs     *ipfslite.Peer
+	ipfsHost host.Host
 }
 
 func NewFileSystem(ctx context.Context, db *badger.DB, ipfsPort int, ipfsDomain string) (*FileSystem, error) {
-	ipfs, err := ipfs2.MakeIPFS(ctx, db, ipfsPort, ipfsDomain)
+	ipfs, host, err := ipfs2.MakeIPFS(ctx, db, ipfsPort, ipfsDomain)
 	if err != nil {
 		return nil, err
 	}
-	return &FileSystem{db: db, ipfs: ipfs}, nil
+	return &FileSystem{db: db, ipfs: ipfs, ipfsHost: host}, nil
 }
 
 func (f *FileSystem) Close() {
