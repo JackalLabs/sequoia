bd6c12e41d7a83ca3db3898ece2880a22bce148b
dahn510 <dahn510@gmail.com>
2025-05-05 18:41:35 -0700

    clean up

--- queue/worker_test.go
+++ queue/worker_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/JackalLabs/sequoia/config"
 	sequoiaWallet "github.com/JackalLabs/sequoia/wallet"
 	wtypes "github.com/desmos-labs/cosmos-go-wallet/types"
+	"github.com/desmos-labs/cosmos-go-wallet/wallet"
 
 	"github.com/JackalLabs/sequoia/testutil/mocks"
 
@@ -28,7 +29,7 @@ import (
 	authTypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
-func TestWorkerSendMaxRetry(t *testing.T) {
+func setupWalletClient(t *testing.T) (*wallet.Wallet, *mocks.MockAuthQueryClient, *mocks.MockServiceClient, *mocks.MockRPCClient) {
 	r := require.New(t)
 
 	chainCfg := wtypes.ChainConfig{
@@ -76,6 +77,16 @@ func TestWorkerSendMaxRetry(t *testing.T) {
 		NodeInfo: p2p.DefaultNodeInfo{Network: "jackaaaal"},
 	}
 	rpcClient.EXPECT().Status(gomock.Any()).Return(&re, nil).AnyTimes()
+	wallet.Client.RPCClient = rpcClient
+
+	return wallet, queryClient, serviceClient, rpcClient
+}
+
+func TestWorkerSendMaxRetry(t *testing.T) {
+	r := require.New(t)
+
+	wallet, _, _, rpcClient := setupWalletClient(t)
+
 	rpcClient.EXPECT().BroadcastTxCommit(gomock.Any(), gomock.Any()).Return(nil, status.Error(codes.Aborted, "some error")).AnyTimes()
 	wallet.Client.RPCClient = rpcClient
 
@@ -104,52 +115,7 @@ func TestWorkerSendMaxRetry(t *testing.T) {
 func TestBatchFullSend(t *testing.T) {
 	r := require.New(t)
 
-	chainCfg := wtypes.ChainConfig{
-		RPCAddr:       "http://localhost:26657",
-		GRPCAddr:      "localhost:9090",
-		GasPrice:      "0.02ujkl",
-		GasAdjustment: 1.5,
-		Bech32Prefix:  "jkl",
-	}
-
-	s := &config.Seed{
-		SeedPhrase:     "forward service profit benefit punch catch fan chief jealous steel harvest column spell rude warm home melody hat broccoli pulse say garlic you firm",
-		DerivationPath: "m/44'/118'/0'/0/0",
-	}
-
-	wallet, err := sequoiaWallet.CreateWallet(s.SeedPhrase, s.DerivationPath, chainCfg)
-	r.NoError(err)
-
-	queryClient := mocks.SetupAuthClient(t)
-
-	baseAcc := authTypes.ProtoBaseAccount()
-	err = baseAcc.SetAddress(sdkTypes.AccAddress(wallet.AccAddress()))
-	r.NoError(err)
-	any, err := codectypes.NewAnyWithValue(baseAcc)
-	r.NoError(err)
-	queryResp := &authTypes.QueryAccountResponse{
-		Account: any,
-	}
-	queryClient.EXPECT().Account(gomock.Any(), &authTypes.QueryAccountRequest{Address: wallet.AccAddress()}).Return(queryResp, nil).AnyTimes()
-
-	wallet.Client.AuthClient = queryClient
-
-	serviceClient := mocks.SetupServiceClient(t)
-
-	// auto gas & fees
-	simRes := txTypes.SimulateResponse{
-		GasInfo: &sdkTypes.GasInfo{GasWanted: 0, GasUsed: 0},
-		Result:  nil,
-	}
-	serviceClient.EXPECT().Simulate(gomock.Any(), gomock.Any()).Return(&simRes, nil).AnyTimes()
-	wallet.Client.TxClient = serviceClient
-
-	rpcClient := mocks.SetupRPCClient(t)
-	re := coretypes.ResultStatus{
-		NodeInfo: p2p.DefaultNodeInfo{Network: "jackaaaal"},
-	}
-	rpcClient.EXPECT().Status(gomock.Any()).Return(&re, nil).AnyTimes()
-	wallet.Client.RPCClient = rpcClient
+	wallet, _, _, rpcclient := setupWalletClient(t)
 
 	wg := sync.WaitGroup{}
 	wg.Add(2)
@@ -174,7 +140,59 @@ func TestBatchFullSend(t *testing.T) {
 
 	var actual ttypes.Tx
 
-	rpcClient.
+	rpcclient.
+		EXPECT().
+		BroadcastTxCommit(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, tx ttypes.Tx) (*sdkTypes.TxResponse, error) {
+			actual = tx
+
+			return nil, status.Error(codes.OK, "no error")
+		}).
+		AnyTimes()
+	wallet.Client.RPCClient = rpcclient
+
+	chMsg := make(chan *Message)
+	w := newWorker(0, wallet, 3, 2, 2, chMsg)
+	go w.start()
+
+	chMsg <- &m0
+	chMsg <- &m1
+
+	wg.Wait()
+
+	close(chMsg)
+	r.EqualValues(expectedSignedTx, actual)
+}
+
+func TestBatchHalfFullSend(t *testing.T) {
+	r := require.New(t)
+
+	wallet, _, _, rpcclient := setupWalletClient(t)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	m0 := Message{
+		msg: types.NewMsgPostProof("1", []byte(""), "", 0, []byte(""), []byte(""), 0),
+		wg:  &wg,
+	}
+	m1 := Message{
+		msg: types.NewMsgPostProof("2", []byte(""), "", 0, []byte(""), []byte(""), 0),
+		wg:  &wg,
+	}
+
+	txData := wtypes.NewTransactionData(m0.msg, m1.msg).WithFeeAuto().WithGasAuto()
+
+	txEncoder := tx.DefaultTxEncoder()
+	builder, err := wallet.BuildTx(txData)
+	r.NoError(err)
+
+	expectedSignedTx, err := txEncoder(builder.GetTx())
+	r.NoError(err)
+
+	var actual ttypes.Tx
+
+	rpcclient.
 		EXPECT().
 		BroadcastTxCommit(gomock.Any(), gomock.Any()).
 		DoAndReturn(func(_ context.Context, tx ttypes.Tx) (*sdkTypes.TxResponse, error) {
@@ -185,7 +203,7 @@ func TestBatchFullSend(t *testing.T) {
 		AnyTimes()
 
 	chMsg := make(chan *Message)
-	w := newWorker(0, wallet, 3, 2, 2, chMsg)
+	w := newWorker(0, wallet, 3, 5, 2, chMsg)
 	go w.start()
 
 	chMsg <- &m0
